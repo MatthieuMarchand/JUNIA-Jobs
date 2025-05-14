7c6eed39c30b0a2ef801e32b0a9a6929cd977c80
Matthieu <matthieu.marchand@mmibordeaux.com>
2025-05-14 16:08:11 +0200

    Ajout de Bootstrap css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-import './bootstrap';
+import "bootstrap"; // la librairie css
+
+import axios from "axios";
+window.axios = axios;
+
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";

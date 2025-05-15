fdbfebb226c584eddbb07efa38752d2eda9eaea4
Matthieu <matthieu.marchand@mmibordeaux.com>
2025-05-15 09:37:02 +0200

    Initialisation du front

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>
+        @hasSection('title')
+            @yield('title') | JUNIA Jobs
+        @else
+            JUNIA Jobs
+        @endif
+    </title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+    @vite(['resources/css/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    @include('components.navbar')
+
+    <main>
+        @yield('content')
+    </main>
+</body>
+</html>

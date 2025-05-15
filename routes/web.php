361351ecfc6bc010cdae1aa5bf90c006bcd1f0e1
Matthieu <matthieu.marchand@mmibordeaux.com>
2025-05-15 09:37:46 +0200

    Initialisation du front

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Student\StudentProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 // Compte

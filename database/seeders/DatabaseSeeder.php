ddcfc680304a001b64ac4620b2f25952eb7ef8e4
LeoDumas <leo.ndumas980@gmail.com>
2025-05-16 14:27:04 +0200

    Feat: Seeder students profiles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\StudentProfile;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -14,9 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
+        User::factory()->student()->create([
             'email' => 'test@example.com',
             'password' => 'password',
         ]);
+
+        StudentProfile::factory()->count(10)->create();
     }
 }
